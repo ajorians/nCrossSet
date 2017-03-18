19f9478c4c6e8e33ddd476f535df21a0e4183e86
A.J. Orians <ajorians@gmail.com>
2017-03-18 19:36:29 -0400

    Added some more levels.

--- src/Levels.c
+++ src/Levels.c
@@ -28,8 +28,8 @@ void LevelLoad(char* pstr, int nLevelNum)
    LEVEL(19);
    LEVEL(20);
    LEVEL(21);
-   /*LEVEL(22);
-   LEVEL(23);*/
+   LEVEL(22);
+   LEVEL(23);
    LEVEL(24);
    LEVEL(25);
    LEVEL(26);
